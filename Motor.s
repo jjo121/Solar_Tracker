887e4153c1bdff17cddb5828e19dba41f079e5ee
jmmaka <149779696+jmmaka@users.noreply.github.com>
2023-12-14 15:06:01 +0000

    Update Motor.s

--- Motor.s
+++ Motor.s
@@ -1,14 +1,16 @@
 #include <xc.inc>
 
 extrn	delayseveral5us, delay_ms
-extrn   posEW
+extrn   pos_xz
     
-global  motorsetup, move_xz, move_xy_anticlockwise, move_xy_clockwise, 
+global  motorsetup, move_xz, move_xy_anticlockwise, move_xy_clockwise 
 global count, tempStore, stop ;to remove when operation verified
-
+global move_xz_anticlockwise, move_xz_clockwise
+    
 psect udata_acs ;reserves space in RAM
 
 count:         ds 1
+count2:	       ds 1
 tempStore:     ds 1
     
 psect	adc_code, class=CODE
@@ -18,19 +20,21 @@ motorsetup:
     movwf	TRISC			; portc is output
     movlw	0x01
     movwf	count
+    movlw	0x0A
+    movwf	count2
     return
 
     
 move_xz:
-    movlw	0xB4
+    movlw	0xFF
     movwf	tempStore
-    movf	posEW, W
+    movf	pos_xz, W
     subwf	tempStore
     bsf		TRISC, 0
     bsf		LATC1
     movlw	0x01
     call	delay_ms
-    movf	posEW, W
+    movf	pos_xz, W
     call	delayseveral5us
     bcf		LATC1
     movlw	0x11     
@@ -40,38 +44,50 @@ move_xz:
     movf	tempStore, W
     call	delayseveral5us
     decfsz	count
-    bra		move_xz
-    movlw	0x0A
+    ;bra		move_xz
+    movlw	0x01
     movwf	count
-    bcf		TRISC, 0
+    ;bcf		TRISC, 0
     return
+
+move_xz_anticlockwise:
+	movlw	0x01	;1
+	cpfseq	pos_xz
+	decf	pos_xz
+	call	move_xz
+	return
+
+move_xz_clockwise:
+	movlw	0xFF	;179
+	cpfseq	pos_xz
+	incf	pos_xz
+	call	move_xz
+	return   
     
 move_xy_anticlockwise:
     bsf		TRISC, 0
     bsf		LATC2
     movlw	0x01
     call	delay_ms
-    movlw	0x55
+    movlw	0x50
     call	delayseveral5us
     bcf		LATC2
-    movlw	0x0C     
+    movlw	0xFF     
     call	delay_ms
     return
+
 
 move_xy_clockwise:
     bsf		TRISC, 0
     bsf		LATC2
     movlw	0x01
     call	delay_ms
-    movlw	0x5C
+    movlw	0x6A
     call	delayseveral5us
     bcf		LATC2
     movlw	0xFF     
     call	delay_ms
-    movlw	0xFF     
-    call	delay_ms
-    bcf		TRISC, 0
-    
+    return
 
 stop:
     bsf		TRISC, 0
@@ -84,6 +100,7 @@ stop:
     movlw	0x0C     
     call	delay_ms    
     return
+    end
     
     
 
