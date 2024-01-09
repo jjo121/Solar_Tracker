c4b0591b6b8672dc71496e7cd5ecbd97c95acfda
jmmaka <149779696+jmmaka@users.noreply.github.com>
2024-01-09 09:57:30 +0000

    Update ADC.s

--- ADC.s
+++ ADC.s
@@ -4,8 +4,9 @@ global  ADC_Setup0,ADC_Setup1,ADC_Setup2,ADC_Setup3,ADC_Setup4, ADC_Read
     
 psect	adc_code, class=CODE
 ;Set (0x30) for testing purposes if VDD doesn't work 
-;Add to ADC file
-ADC_Setup0:
+;Contains procedures which allow set ups of pins of PORTA for ADC input
+;Also contians reading procedure
+ADC_Setup0:		    ;Set up ADC to read pin 0-Photodiode 0
 	bsf	TRISA, PORTA_RA0_POSN, A  ; pin RA0==AN0 input
 	movlb	0x0f
 	bsf	ANSEL0	    ; set AN0 to analog
@@ -18,7 +19,7 @@ ADC_Setup0:
 	movwf   ADCON2, A   ; Fosc/64 clock and acquisition times
 	return
 
-ADC_Setup1:
+ADC_Setup1:                 ;Set up ADC to read pin 1-Photodiode 1
 	bsf	TRISA, PORTA_RA1_POSN, A  ; pin RA0==AN0 input
 	movlb	0x0f
 	bsf	ANSEL1	    ; set AN0 to analog
@@ -31,7 +32,7 @@ ADC_Setup1:
 	movwf   ADCON2, A   ; Fosc/64 clock and acquisition times
 	return	
 
-ADC_Setup2:
+ADC_Setup2:		    ;Set up ADC to read pin 2-Photodiode 2
 	bsf	TRISA, PORTA_RA2_POSN, A  ; pin RA0==AN0 input
 	movlb	0x0f
 	bsf	ANSEL2	    ; set AN0 to analog
@@ -44,7 +45,7 @@ ADC_Setup2:
 	movwf   ADCON2, A   ; Fosc/64 clock and acquisition times
 	return	
 
-ADC_Setup3:
+ADC_Setup3:		    ;Set up ADC to read pin 3-Photodiode 3
 	bsf	TRISA, PORTA_RA3_POSN, A  ; pin RA0==AN0 input
 	movlb	0x0f
 	bsf	ANSEL3	    ; set AN0 to analog
@@ -56,7 +57,7 @@ ADC_Setup3:
 	movlw   0xF6	    ; Right justified output
 	movwf   ADCON2, A   ; Fosc/64 clock and acquisition times
 	return
-ADC_Setup4:
+ADC_Setup4:                 ;Fifth ADC read for Solar Panel if necessary
 	bsf	TRISA, PORTA_RA4_POSN, A  ; pin RA0==AN0 input
 	movlb	0x0f
 	bsf	ANSEL4	    ; set AN0 to analog
